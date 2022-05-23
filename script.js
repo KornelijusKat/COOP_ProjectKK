d7535ad4812835418fb2db9acdbd16e4cae3e6f2
Donatas Vitkus <vitk.donatas@gmail.com>
2022-05-23 22:19:54 +0300

    Login from carcass lil change

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Get the modal
 var modal = document.getElementById('id01');
 
+let myButton = document.querySelector("#loginbtn");  myButton.style.position = "absolute";
+myButton.style.left = "50%";
+myButton.style.transform = "translateX(-50%)";
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-    let myButton = document.querySelector("#loginbtn");  myButton.style.position = "absolute";
-myButton.style.left = "50%";
-myButton.style.transform = "translateX(-50%)";
 }
