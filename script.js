610291f3bac6a11955272ed25703b58d29804b8d
Donatas Vitkus <vitk.donatas@gmail.com>
2022-05-23 22:53:39 +0300

    Registration form shell

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Login form shell
+
 // Get the modal
 var modal = document.getElementById('id01');
 
-let myButton = document.querySelector("#loginbtn");  myButton.style.position = "absolute";
-myButton.style.left = "50%";
-myButton.style.transform = "translateX(-50%)";
+let myloginButton = document.querySelector("#loginbtn");  myloginButton.style.position = "absolute";
+myloginButton.style.left = "47%";
+myloginButton.style.transform = "translateX(-50%)";
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -11,3 +13,19 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Registration from shell
+
+// Get the modal
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+let registrationButton = document.querySelector("#signupbtn");  registrationButton.style.position = "absolute";
+registrationButton.style.left = "53%";
+registrationButton.style.transform = "translateX(-50%)";
