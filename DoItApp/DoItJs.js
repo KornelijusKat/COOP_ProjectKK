160af9b511aaa596a96f28a7c17c169572f496c7
KornelijusKat <katinaskornelijus@gmail.com>
2022-05-30 17:48:40 +0300

    Fixing up work,logout button

--- DoItApp/DoItJs.js
+++ DoItApp/DoItJs.js
@@ -5,22 +5,32 @@ const secondInput = document.createElement('input');
 const thirdInput = document.createElement('input');
 const submitter = document.createElement('input');
 const submitter2 = document.createElement('input');
-const snewForm = document.createElement('form');
-const sfirstInput = document.createElement('input');
-const ssecondInput = document.createElement('input');
-const sthirdInput = document.createElement('input');
+const editForm = document.createElement('form');
+const editInput1 = document.createElement('input');
+const editInput2 = document.createElement('input');
+const editInput3 = document.createElement('input');
 const container = document.querySelector('.container');
 const addformlocation = document.querySelector('#Form');
 const userhead3 = document.querySelector('#ShowUser');
+const logout = document.querySelector('#Logout');
 let placeholder;
 submitter2.setAttribute('type','submit');
 submitter.setAttribute("type","submit");
 //button click that shows a form to submit for posting to database
 AddButton.addEventListener('click',function(event){
+    addformlocation.innerHTML = "";
     createForm(submitter);
+    newForm.reset();
+})
+logout.addEventListener('click',function(event){
+  localStorage.clear();
+  history.back();
 })
 //creates form for Add button
 function createForm(submit){ 
+    firstInput.placeholder = "type";
+    secondInput.placeholder = "content";
+    thirdInput.placeholder = "endDate";
     newForm.append(firstInput,secondInput,thirdInput,submit);
     addformlocation.append(newForm);
 }
@@ -35,7 +45,7 @@ function AddPost(){
           type: firstInput.value,
           content: secondInput.value,
           endDate: thirdInput.value, 
-          User: JSON.stringify(art[0]['name']).replaceAll('"',"")
+          User: JSON.stringify(art[0]['name']).replaceAll('"',"") + JSON.stringify(art[0]['lastname']).replaceAll('"',"")
         })
       })
         .then((response) => {
@@ -44,6 +54,7 @@ function AddPost(){
             return response.json();
          } else {
             console.log('not okay');
+            alert("not all inputs have been filled entirely/correctly")
           }
         })  .then((result) => {
           console.log(result);
@@ -51,30 +62,35 @@ function AddPost(){
       .then((result) => {
         addformlocation.innerHTML = "";
         container.innerHTML = "";
-        GetRecordsOfUser()
+        ShowRecords();
      })
       .catch((err) => {
         console.log(err);  
     })  
 }
 //returns all records by user(ps.change filter to user)
-function GetRecordsOfUser(){
-    fetch('https://testapi.io/api/KornelKat/resource/SecondDatabase')
+let recordfetch;
+ function GetRecordsOfUser(){
+   recordfetch = fetch('https://testapi.io/api/KornelKat/resource/SecondDatabase')
         .then((res) => {
             if(res.ok){
               return res.json();
             }
-          })
-          .then(result => { 
-              let it = result.data.filter(({User}) => User === JSON.stringify(art[0]['name']).replaceAll('"',"") )||[];
-              container.innerHTML = "";
-              addformlocation.innerHTML = "";
-              CreateDiv(it);
-        })
-       }  
+          }).catch((error) => console.log(error));
+        }  
+//Gets Data and Shows it
+function ShowRecords(){
+  GetRecordsOfUser();
+  recordfetch.then(result => { 
+    let it = result.data.filter(({User}) => User === (JSON.stringify(art[0]['name']).replaceAll('"',"") + JSON.stringify(art[0]['lastname']).replaceAll('"',"")));
+    container.innerHTML = "";
+    addformlocation.innerHTML = "";
+    CreateDiv(it);
+  })
+}
 //creates Div cards
 function CreateDiv(Records){
-    Records.forEach(element => {
+     Records.forEach(element => {
         const DivCard = document.createElement('div');
         const buttondiv = document.createElement('div');
         const paragraph = document.createElement('p');
@@ -83,31 +99,35 @@ function CreateDiv(Records){
         const editButton = document.createElement('button');
         const deleteButton = document.createElement('button');
         deleteButton.addEventListener('click',function(event){
-          const elementID = event.currentTarget.parentElement.id;
+          const elementID =  this.parentNode.parentElement.id;;
           DeleteRecord(elementID,DivCard);
         });
         editButton.addEventListener('click',function(event){
           event.preventDefault();
-          const elementID = event.currentTarget.parentElement.id;
-          placeholder = elementID;
-          EditFields();
+          addformlocation.innerHTML = "";
+          //const elementID = event.currentTarget.parentElement.id;
+          placeholder = this.parentNode.parentElement.id;
+          EditFields(placeholder);        
           submitter2.addEventListener('click',function(event){
             event.preventDefault();
-            console.log(placeholder);
             EditRecord(placeholder);
           })
         })
         console.log(element);
-        const myobjarr = JSON.stringify(element); 
-        paragraph.innerHTML = JSON.stringify(element).replaceAll(',','<br />').replaceAll('}','<br />').replaceAll('{','');
+        paragraph.innerHTML = JSON.stringify(element)
+        .replaceAll(',','<br />')
+          .replaceAll('}','<br />')
+            .replaceAll('{','')
+              .replaceAll('[','')
+                .replaceAll(']','')
+                  .replaceAll('"',' ');
         editButton.className = 'recordbtn';
         editButton.innerHTML = "Edit";
         deleteButton.className = 'recordbtn2';
         deleteButton.innerHTML = 'Delete';
-        
-        DivCard.append(paragraph,buttondiv,editButton,deleteButton);
+        buttondiv.append(editButton,deleteButton);
+        DivCard.append(paragraph,buttondiv);
         DivCard.setAttribute('id',element.id);
-        
         container.append(DivCard)});
 }  
 //Deletes selected div card
@@ -119,9 +139,7 @@ async function DeleteRecord(RecordID,DivCard){
     },
   })
   if(deleter){
-    // DivCard.innerHTML ="";
-    // DivCard.style.border ="none";
-    GetRecordsOfUser();
+   ShowRecords();
   }
 }      
 // Click event that sends input values to Database
@@ -131,34 +149,38 @@ AddPost();
 })
 //Changes the selected record of user, ps(need to change the inner html part to a new created main or section maybe)
 async function EditRecord(Userid){
- const editing =  await fetch(`https://testapi.io/api/KornelKat/resource/SecondDatabase/` + Userid, {
+ const editing =  await fetch(`https://testapi.io/api/KornelKat/resource/SecondDatabase/${Userid}`, {
     method: 'PUT',
     headers:{
       'Content-type': 'application/json',
     },
     body: JSON.stringify({
-      'type': sfirstInput.value,
-      'content': ssecondInput.value,
-      'endDate' : sthirdInput.value,
-      'User' :  JSON.stringify(art[0]['name']).replaceAll('"',"")
+      'type': editInput1.value,
+      'content': editInput2.value,
+      'endDate' : editInput3.value,
+      'User' :  (JSON.stringify(art[0]['name']).replaceAll('"',"") + JSON.stringify(art[0]['lastname']).replaceAll('"',""))
     })
   })
-  if(editing){
-   
-    GetRecordsOfUser();
-  }
+    if(editing){
+    ShowRecords();
+    }
 }
-GetRecordsOfUser()
+ShowRecords()
 //Editing form for input
-function EditFields(){
-snewForm.append(sfirstInput,ssecondInput,sthirdInput,submitter2);
-addformlocation.append(snewForm);
+function EditFields(placeholder){
+cardcontent(placeholder);
+editForm.append(editInput1,editInput2,editInput3,submitter2);
+addformlocation.append(editForm);
 }
-//returns user from localstorage(also maybe need to change name part if doesnt get localstorage item)
-function GetUser(){
-  const getName = localStorage.getItem('name')||[];
-  return getName;
-}
-
 let art = JSON.parse(localStorage.getItem("User"));
-userhead3.innerHTML +=  JSON.stringify(art[0]['name'] + JSON.stringify(art[0]['lastname']));
+userhead3.innerHTML +=  JSON.stringify(art[0]['name']+ " " + JSON.stringify(art[0]['lastname'])).replaceAll("\\","").replaceAll('"','');
+function cardcontent(eleId){ 
+  GetRecordsOfUser();
+  recordfetch.then(result => {
+  let editingobj = result.data.filter(({id}) => id === Number(eleId)) 
+  let objarray = Object.values(editingobj);
+  editInput1.value = objarray[0]['type'];
+  editInput2.value = objarray[0]['content'];
+  editInput3.value = objarray[0]['endDate'];
+  }
+)}
